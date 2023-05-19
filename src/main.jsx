5515b47c38083967ec8b60e886cc58a04e46381c
imason5 <ianmason5@googlemail.com>
2023-05-19 11:00:47 +0100

    Add chakraui

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import SessionContextProvider from "./contexts/SessionContext.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<SessionContextProvider>
-				<App />
-			</SessionContextProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <SessionContextProvider>
+          <App />
+        </SessionContextProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
--- src/pages/DailyDiary.jsx
+++ src/pages/DailyDiary.jsx
@@ -1,5 +1,102 @@
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  VStack,
+  HStack,
+  Select,
+  Input,
+  IconButton,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+} from "@chakra-ui/react";
+import { SearchIcon, AddIcon } from "@chakra-ui/icons";
+
 function DailyDiary() {
-	return <div>DailyDiary</div>;
+  const [mealType, setMealType] = useState("Breakfast");
+  const [foodName, setFoodName] = useState("");
+  const [frequentFoods, setFrequentFoods] = useState([]);
+
+  const handleScanBarcode = () => {
+    // TODO: Implement barcode scanning
+    console.log("Barcode scanning not implemented yet");
+  };
+
+  const handleMealTypeChange = (event) => {
+    setMealType(event.target.value);
+  };
+
+  const handleFoodNameChange = (event) => {
+    setFoodName(event.target.value);
+  };
+
+  const handleFrequentFoodChange = (event) => {
+    const selectedFood = event.target.value;
+    const index = frequentFoods.indexOf(selectedFood);
+
+    if (index > -1) {
+      // If the food is already in the list, remove it
+      setFrequentFoods(frequentFoods.filter((food) => food !== selectedFood));
+    } else {
+      // Otherwise, add the food to the list
+      setFrequentFoods([...frequentFoods, selectedFood]);
+    }
+  };
+
+  const removeFrequentFood = (removedFood) => {
+    setFrequentFoods(frequentFoods.filter((food) => food !== removedFood));
+  };
+
+  return (
+    <VStack align="stretch" spacing={4}>
+      <Select
+        placeholder="Meal type"
+        value={mealType}
+        onChange={handleMealTypeChange}
+      >
+        <option value="Breakfast">Breakfast</option>
+        <option value="Lunch">Lunch</option>
+        <option value="Dinner">Dinner</option>
+        <option value="Snack">Snack</option>
+      </Select>
+
+      <HStack>
+        <Input
+          placeholder="Enter food name"
+          value={foodName}
+          onChange={handleFoodNameChange}
+        />
+        <IconButton aria-label="Search food" icon={<SearchIcon />} />
+      </HStack>
+
+      <Button leftIcon={<AddIcon />} onClick={handleScanBarcode}>
+        Scan Barcode
+      </Button>
+
+      <HStack wrap="wrap" spacing={2}>
+        {frequentFoods.map((food) => (
+          <Tag
+            size="lg"
+            key={food}
+            borderRadius="full"
+            variant="solid"
+            colorScheme="teal"
+          >
+            <TagLabel>{food}</TagLabel>
+            <TagCloseButton onClick={() => removeFrequentFood(food)} />
+          </Tag>
+        ))}
+      </HStack>
+
+      <Box as="form" onSubmit={handleFrequentFoodChange}>
+        <HStack>
+          <Input placeholder="Enter frequent food name" />
+          <Button type="submit">Add to frequent foods</Button>
+        </HStack>
+      </Box>
+    </VStack>
+  );
 }
 
 export default DailyDiary;
