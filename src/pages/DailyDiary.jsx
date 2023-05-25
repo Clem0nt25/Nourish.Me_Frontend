0bcc6fba3eb3f6d2af3784b227b8ce6243132100
imason5 <ianmason5@googlemail.com>
2023-05-25 17:02:00 +0100

    Add fix for crash on login

--- src/pages/DailyDiary.jsx
+++ src/pages/DailyDiary.jsx
@@ -12,167 +12,167 @@ import { DailyMacros } from "../components/DailyDiary/DailyMacros";
 // import barcodeIcon from "../assets/barcode.png";
 import { useNavigate } from "react-router-dom";
 import {
-	useGetFood,
-	useFetchDiary,
-	useFetchUserSpecs,
-	useFetchUserSpecsCurr,
-	useDeleteFood,
+  useGetFood,
+  useFetchDiary,
+  useFetchUserSpecs,
+  useFetchUserSpecsCurr,
+  useDeleteFood,
 } from "../services/foodService.js";
 import { delay } from "../utils/delay";
 
 function DailyDiary() {
-	const { currUserSt } = useContext(SessionContext);
-	const [userSpecsSt, setUserSpecsSt] = useState();
-	const [foodName, setFoodName] = useState("");
-	const [results, setResults] = useState([]);
-	const [diary, setDiary] = useState({
-		breakfast: [],
-		lunch: [],
-		dinner: [],
-		snack: [],
-	});
-	const [isLoading, setIsLoading] = useState(false);
-	const [currentDay, setCurrentDay] = useState("");
+  const { currUserSt } = useContext(SessionContext);
+  const [userSpecsSt, setUserSpecsSt] = useState();
+  const [foodName, setFoodName] = useState("");
+  const [results, setResults] = useState([]);
+  const [diary, setDiary] = useState({
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+    snack: [],
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentDay, setCurrentDay] = useState("");
 
-	const searchRef = useRef(null);
-	const navigate = useNavigate();
-	const location = useLocation();
-	const getFood = useGetFood();
-	const fetchDiary = useFetchDiary();
-	const fetchUserSpecs = useFetchUserSpecs();
-	const fetchUserSpecsCurr = useFetchUserSpecsCurr();
-	const deleteFood = useDeleteFood();
+  const searchRef = useRef(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const getFood = useGetFood();
+  const fetchDiary = useFetchDiary();
+  const fetchUserSpecs = useFetchUserSpecs();
+  const fetchUserSpecsCurr = useFetchUserSpecsCurr();
+  const deleteFood = useDeleteFood();
 
-	useEffect(() => {
-		const fetchInitialDiary = async () => {
-			const diary =
-				location.state?.newDiary || (await fetchDiary(currUserSt._id));
-			console.log("Diary data fetched in DailyDiary:", diary);
-			setDiary(diary);
-		};
+  useEffect(() => {
+    const fetchInitialDiary = async () => {
+      const diary =
+        location.state?.newDiary || (await fetchDiary(currUserSt._id));
+      console.log("Diary data fetched in DailyDiary:", diary);
+      setDiary(diary);
+    };
 
-		fetchInitialDiary();
-	}, []);
+    fetchInitialDiary();
+  }, [currUserSt._id]);
 
-	useEffect(() => {
-		const fetchCurrentUserSpec = async () => {
-			try {
-				console.log("currUserSt._id:", currUserSt._id);
-				await fetchUserSpecsCurr(currUserSt._id);
-				const userSpecs = await fetchUserSpecs(currUserSt._id);
-				console.log("User specs fetched in DailyDiary:", userSpecs);
-				setUserSpecsSt(userSpecs);
-			} catch (error) {
-				if (error.response.status === 404) {
-					navigate("/progress-questionnaire");
-				} else {
-					console.error("Error fetching user specs:", error);
-				}
-			}
-		};
+  useEffect(() => {
+    const fetchCurrentUserSpec = async () => {
+      try {
+        console.log("currUserSt._id:", currUserSt._id);
+        await fetchUserSpecsCurr(currUserSt._id);
+        const userSpecs = await fetchUserSpecs(currUserSt._id);
+        console.log("User specs fetched in DailyDiary:", userSpecs);
+        setUserSpecsSt(userSpecs);
+      } catch (error) {
+        if (error.response.status === 404) {
+          navigate("/progress-questionnaire");
+        } else {
+          console.error("Error fetching user specs:", error);
+        }
+      }
+    };
 
-		fetchCurrentUserSpec();
-	}, []);
+    fetchCurrentUserSpec();
+  }, [currUserSt._id]);
 
-	const searchFoods = async (isRetry = false) => {
-		setIsLoading(true);
+  const searchFoods = async (isRetry = false) => {
+    setIsLoading(true);
 
-		if (!foodName.trim()) {
-			setResults([]);
-			setIsLoading(false);
-			return;
-		}
+    if (!foodName.trim()) {
+      setResults([]);
+      setIsLoading(false);
+      return;
+    }
 
-		try {
-			const response = await getFood(foodName);
-			setResults(response);
-		} catch (error) {
-			console.log(error);
-			if (!isRetry) {
-				console.log("Error occurred. Trying again in 1 second...");
-				setTimeout(() => searchFoods(true), 1000);
-			} else {
-				console.log("Error occurred again. Aborting.");
-			}
-		} finally {
-			setIsLoading(false);
-		}
-	};
+    try {
+      const response = await getFood(foodName);
+      setResults(response);
+    } catch (error) {
+      console.log(error);
+      if (!isRetry) {
+        console.log("Error occurred. Trying again in 1 second...");
+        setTimeout(() => searchFoods(true), 1000);
+      } else {
+        console.log("Error occurred again. Aborting.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-	const handleFoodSelect = (food) => {
-		navigate(`/food-details/${food.barcode}`, {
-			state: { selectedFood: food },
-		});
-	};
-	useEffect(() => {
-		return () => {
-			clearTimeout(searchRef.current);
-		};
-	}, []);
-	const handleInputChange = (e) => {
-		setFoodName(e.target.value);
-		if (searchRef.current) clearTimeout(searchRef.current);
-		searchRef.current = delay(searchFoods, 300);
-	};
+  const handleFoodSelect = (food) => {
+    navigate(`/food-details/${food.barcode}`, {
+      state: { selectedFood: food },
+    });
+  };
+  useEffect(() => {
+    return () => {
+      clearTimeout(searchRef.current);
+    };
+  }, []);
+  const handleInputChange = (e) => {
+    setFoodName(e.target.value);
+    if (searchRef.current) clearTimeout(searchRef.current);
+    searchRef.current = delay(searchFoods, 300);
+  };
 
-	const handleDeleteFood = async (userId, barcode, mealId, currentDate) => {
-		await deleteFood(userId, barcode, mealId, currentDate);
-		const updatedDiary = await fetchDiary(currUserSt._id);
-		setDiary(updatedDiary);
-		// fetch updated user specs
-		const updatedUserSpecs = await fetchUserSpecs(userId);
-		setUserSpecsSt(updatedUserSpecs);
-	};
+  const handleDeleteFood = async (userId, barcode, mealId, currentDate) => {
+    await deleteFood(userId, barcode, mealId, currentDate);
+    const updatedDiary = await fetchDiary(currUserSt._id);
+    setDiary(updatedDiary);
+    // fetch updated user specs
+    const updatedUserSpecs = await fetchUserSpecs(userId);
+    setUserSpecsSt(updatedUserSpecs);
+  };
 
-	// const handleScanBarcode = () => {
-	//   console.log("Barcode scanning not implemented yet");
-	// };
+  // const handleScanBarcode = () => {
+  //   console.log("Barcode scanning not implemented yet");
+  // };
 
-	useEffect(() => {
-		return () => {
-			clearTimeout(searchRef.current);
-		};
-	}, []);
+  useEffect(() => {
+    return () => {
+      clearTimeout(searchRef.current);
+    };
+  }, []);
 
-	useEffect(() => {
-		const currentDate = new Date();
-		const options = { weekday: "long" };
-		const currentDayOfWeek = currentDate.toLocaleDateString("en-US", options);
-		setCurrentDay(currentDayOfWeek);
-	}, []);
+  useEffect(() => {
+    const currentDate = new Date();
+    const options = { weekday: "long" };
+    const currentDayOfWeek = currentDate.toLocaleDateString("en-US", options);
+    setCurrentDay(currentDayOfWeek);
+  }, []);
 
-	return (
-		<MainContainer>
-			<VStack align="stretch" spacing={4}>
-				<Center>
-					<Heading mb={1}>{currentDay}</Heading>
-				</Center>
+  return (
+    <MainContainer>
+      <VStack align="stretch" spacing={4}>
+        <Center>
+          <Heading mb={1}>{currentDay}</Heading>
+        </Center>
 
-				{/* Display daily macros */}
+        {/* Display daily macros */}
 
-				<Box>
-					<DailyMacros userSpecsSt={userSpecsSt} />
-				</Box>
+        <Box>
+          <DailyMacros userSpecsSt={userSpecsSt} />
+        </Box>
 
-				{/* Search bar */}
-				<FoodSearchBar
-					foodName={foodName}
-					handleInputChange={handleInputChange}
-				/>
+        {/* Search bar */}
+        <FoodSearchBar
+          foodName={foodName}
+          handleInputChange={handleInputChange}
+        />
 
-				{/* Display loading state */}
-				<LoadingIndicator isLoading={isLoading} />
+        {/* Display loading state */}
+        <LoadingIndicator isLoading={isLoading} />
 
-				{/* Display search results */}
-				<FoodSearchResults
-					results={results}
-					handleFoodSelect={handleFoodSelect}
-				/>
+        {/* Display search results */}
+        <FoodSearchResults
+          results={results}
+          handleFoodSelect={handleFoodSelect}
+        />
 
-				{/* Display diary entries */}
-				<FoodDiary diary={diary} onDeleteFood={handleDeleteFood} />
+        {/* Display diary entries */}
+        <FoodDiary diary={diary} onDeleteFood={handleDeleteFood} />
 
-				{/* <Center>
+        {/* <Center>
           <IconButton
             variant="button-primary"
             aria-label="Scan a barcode"
@@ -184,8 +184,8 @@ function DailyDiary() {
             _hover={{ boxShadow: "xl" }}
           />
         </Center> */}
-			</VStack>
-		</MainContainer>
-	);
+      </VStack>
+    </MainContainer>
+  );
 }
 export default DailyDiary;
